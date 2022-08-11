6af0afdc837a18adcceb9a23fe314e7e62cddb12
Yasser BOURSSIA <96531894+yasserLMAO@users.noreply.github.com>
2022-08-11 12:34:08 +0200

    Delete MCP9808/software directory

--- MCP9808/software/montimer.h
+++
@@ -1,5 +0,0 @@
-
-int getperiod();
-void start_timer();
-unsigned int getsnap();
-unsigned int getsnapdif(int v1,int v2);
--- MCP9808/software/sprint.h
+++
@@ -1,6 +0,0 @@
-#ifndef __MYSPRINTF_H__
-#define __MYSPRINTF_H__
-
-int sprint(char *out, const char *format, ...);
-
-#endif
